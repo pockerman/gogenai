454b6c7b43f4175f65cce1272a06360192cd2d17
pockerman <a.giavaras@gmail.com>
2026-05-03 18:39:48 +0100

    Second commit

---
+++ sdks/python_sdk/genai_platform.py
@@ -0,0 +1,8 @@
+class GenAIPlatform:
+    def __init__(self, gateway_url str):
+        self.gateway_url = gateway_url
+        self._sessions = None    
+        self._models = None    
+        self._data = None    
+
+    
---
+++ sdks/python_sdk/model_client.py
@@ -0,0 +1,5 @@
+from proto import models_pb2, models_pb2_grpc
+from .base_client import BaseClient
+
+class ModelClient(BaseClient):
+    pass
